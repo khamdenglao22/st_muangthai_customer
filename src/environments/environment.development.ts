3ee04ea7240027b53fcbcb265aa9c01957b5a1db
khamdenglao22 <khamdenglao22@gmail.com>
2024-09-19 10:55:09 +0700

    first commit

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,15 +1,3 @@
 export const environment = {
   production: true,
-  // apiUrl: 'http://19.168.1.22:8000/backoffice/api',
-  // apiUrl: 'http://141.164.110.54:8000/backoffice/api',
-  apiUrl: 'https://www.stmuangthai.com/bof/api',
-  // apiUrl: 'http://localhost:8888/bof/api',
-  // apiUrl: 'http://34.92.144.38:8888/bof/api',
-  public_dev: '',
-  phetsarathUrl: '',
-  baseUrl: '',
-  recaptcha: {
-    siteKey: '6LdvvqQpAAAAAEyjUc390fpO6n18bUFqG7qLTwim',
-    secretKey:'6LdvvqQpAAAAAI8--Xs1B5UnZdIITOSCKE0ZEZMR',
-  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,1 @@
-export const environment = {
-  production: false,
-  // apiUrl: 'http://localhost:8888/bof/api',
-  // apiUrl: "http://141.164.110.54:8000/backoffice/api",
-  // apiUrl: 'https://34.92.144.38/bof/api',
-  apiUrl: 'https://www.stmuangthai.com/bof/api',
-  // apiUrl: 'http://19.168.1.22:8000/backoffice/api',
-
-  recaptcha: {
-    siteKey: '6LcZwKQpAAAAAIW3l-DX4sxEytIHHmi_CKlA-DQ0',
-    secretKey: '6LcZwKQpAAAAAAEvpzm7zF65RoOMJgZvGPfHXEGh',
-  },
-};
+export const environment = {};
